505055cfd300b6e06c4253ac84342a3431cc2fa8
Your Name <you@example.com>
2023-04-07 09:00:56 +0800

    优化性能

--- src/Buffer/Buffer.php
+++ src/Buffer/Buffer.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace BambooX\Sniper\Buffer;
+namespace Sniper\Buffer;
 
 class Buffer
 {
-    public array $buf = [];
+    public string $buf = '';
 
     public int $readIndex = 0;
 
@@ -19,22 +19,7 @@ class Buffer
 
     public function write($str)
     {
-        if ($str instanceof Buffer) {
-            foreach ($str->buf as $hex) {
-                $this->buf[] = $hex;
-                $this->writeIndex++;
-            }
-        } else if (is_string($str)) {
-            $len = strlen($str);
-            for ($i = 0; $i < $len; $i++) {
-                $hex = dechex(ord($str[$i]));
-                if (strlen($hex) < 2) {
-                    $hex = str_pad($hex, 2, 0, STR_PAD_LEFT);
-                }
-                $this->buf[] = $hex;
-                $this->writeIndex++;
-            }
-        }
+        $this->buf .= $str;
     }
 
 
@@ -45,66 +30,35 @@ class Buffer
 
     public function writeInt16(int $num)
     {
-        $str = dechex($num);
-        if (strlen($str) < 4) {
-            $str = str_pad($str, 4, 0, STR_PAD_LEFT);
-        }
-        foreach (str_split($str, 2) as $val) {
-            $this->buf[$this->writeIndex] = $val;
-            $this->writeIndex++;
-        }
+        $hex = 0X00;
+        $this->buf .= chr($num >> 8 | $hex) . chr($num | $hex);
     }
 
     public function writeInt32(int $num)
     {
-        $str = dechex($num);
-        if (strlen($str) < 8) {
-            $str = str_pad($str, 8, 0, STR_PAD_LEFT);
-        }
-        foreach (str_split($str, 2) as $val) {
-            $this->buf[] = $val;
-            $this->writeIndex++;
-        }
+        $hex = 0X00;
+        $this->buf .= chr($num >> 24 | $hex) . chr($num >> 16 | $hex) . chr($num >> 8 | $hex) . chr($num | $hex);
     }
 
     public function writeInt64(int $num)
     {
-        $str = dechex($num);
-        if ($str < 16) {
-            $str = str_pad($str, 16, 0, STR_PAD_LEFT);
-        }
-        foreach (str_split($str, 2) as $val) {
-            $this->buf[] = $val;
-            $this->writeIndex++;
-        }
+        $hex = 0X00;
+        $this->buf .= chr($num >> 56 | $hex).chr($num >> 48 | $hex).chr($num >> 40 | $hex).chr($num >> 32 | $hex).chr($num >> 24 | $hex) . chr($num >> 16 | $hex) . chr($num >> 8 | $hex) . chr($num | $hex);
     }
 
     public function readInt32(): int
     {
-        $byteArr = [];
-        for ($i = 0; $i < 4; $i++) {
-            $byteArr[] = $this->buf[$this->readIndex];
-            $this->readIndex++;
-        }
-        $str = implode('', $byteArr);
-        return hexdec($str);
+
     }
 
     public function readInt64(): int
     {
-        $byteArr = [];
-        for ($i = 0; $i < 8; $i++) {
-            $byteArr[] = $this->buf[$this->readIndex];
-            $this->readIndex++;
-        }
-        $str = implode('', $byteArr);
-        return hexdec($str);
+
     }
 
     public function toString(): bool|string
     {
-        $byteArr = array_slice($this->buf,0,$this->writeIndex);
-        $str = implode('', $byteArr);
-        return hex2bin($str);
+        return $this->buf;
     }
 }
+
