120faf71d79f545cd20a111ca176b28a33a6f7dc
jack <jack@gmail.com>
2023-04-07 14:20:07 +0800

    update

--- src/Buffer/Buffer.php
+++ src/Buffer/Buffer.php
@@ -2,6 +2,8 @@
 
 namespace Sniper\Buffer;
 
+use Sniper\Exceptions\BufferLengthErrorException;
+
 class Buffer
 {
     public string $buf = '';
@@ -46,14 +48,50 @@ class Buffer
         $this->buf .= chr($num >> 56 | $hex).chr($num >> 48 | $hex).chr($num >> 40 | $hex).chr($num >> 32 | $hex).chr($num >> 24 | $hex) . chr($num >> 16 | $hex) . chr($num >> 8 | $hex) . chr($num | $hex);
     }
 
+    public function readInt16():int
+    {
+        if(\strlen($this->buf) < 2) {
+            throw new BufferLengthErrorException();
+        }
+        $num = 0X0000;
+        $num  = $num | (ord($this->buf[2]) << 8);
+        $num  = $num | (ord($this->buf[3]));
+        return $num;
+    }
+
     public function readInt32(): int
     {
-
+        if(\strlen($this->buf) < 4) {
+            throw new BufferLengthErrorException();
+        }
+        $num = 0X00000000;
+        $num  = $num | (ord($this->buf[0]) << 24);
+        $num  = $num | (ord($this->buf[1]) << 16);
+        $num  = $num | (ord($this->buf[2]) << 8);
+        $num  = $num | (ord($this->buf[3]));
+        return $num;
     }
 
     public function readInt64(): int
     {
+        if(\strlen($this->buf) < 8) {
+            throw new BufferLengthErrorException();
+        }
+        $num = 0X0000000000000000;
+        $num  = $num | (ord($this->buf[0]) << 56);
+        $num  = $num | (ord($this->buf[0]) << 48);
+        $num  = $num | (ord($this->buf[0]) << 40);
+        $num  = $num | (ord($this->buf[0]) << 32);
+        $num  = $num | (ord($this->buf[0]) << 24);
+        $num  = $num | (ord($this->buf[1]) << 16);
+        $num  = $num | (ord($this->buf[2]) << 8);
+        $num  = $num | (ord($this->buf[3]));
+        return $num;
+    }
 
+    public function length():int
+    {
+        return \strlen($this->buf);        
     }
 
     public function toString(): bool|string
@@ -62,3 +100,8 @@ class Buffer
     }
 }
 
+
+$buf= new Buffer();
+$buf->writeInt32(34444);
+$num = $buf->readInt32();
+echo $num;
---
+++ src/Exceptions/BufferLengthErrorException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Sniper\Exceptions;
+
+
+class BufferLengthErrorException extends \Exception
+{
+
+}
